c2813bc1b49a6ded9e22132c8082aa0d654681ff
Kyle Recktenwald <kreck30590@gmail.com>
2023-12-18 10:46:30 -0500

    Working Keycloak Token, Still No Spring Contact

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
+import React, { useEffect, createContext, useState } from 'react';
 
 import AllRuns from './pages/AllRuns';
 import RunDetail from './pages/RunDetail';
 import NewRun from './pages/NewRun';
 import NotFound from './pages/NotFound';
 import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import Keycloak from 'keycloak-js';
+
+// Create a context to hold the Keycloak instance
+export const KeycloakContext = createContext();
 
 function App() {
+  const [keycloak, setKeycloak] = useState(null);
+
+  useEffect(() => {
+    const keycloakInstance = new Keycloak({
+      url: 'http://localhost:8180',
+        realm: 'runnersutilapp',
+        clientId: 'runner-utils-app-client',
+    });
+
+    keycloakInstance.init({ onLoad: 'login-required', pkceMethod: "S256" }).then((authenticated) => {
+      if (authenticated) {
+        console.log('User is authenticated');
+      } else {
+        console.log('User is not authenticated');
+      }
+      setKeycloak(keycloakInstance);
+    });
+  }, []);
   return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/runs' />
-        </Route>
-        <Route path='/runs' exact>
-          <AllRuns />
-        </Route>
-        <Route path='/runs/:runId'>
-          <RunDetail />
-        </Route>
-        <Route path='/new-run'>
-          <NewRun />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Layout>
+    <KeycloakContext.Provider value={keycloak}>
+      <Layout>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/home' exact>
+            <Home />
+          </Route>
+          <Route path='/runs' exact>
+            <AllRuns />
+          </Route>
+          <Route path='/runs/:runId'>
+            <RunDetail />
+          </Route>
+          <Route path='/new-run'>
+            <NewRun />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Layout>
+    </KeycloakContext.Provider>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,52 @@
-import React from "react"
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { KeycloakContext } from '../App';
 
-const Home = (props) => {
-    const { loggedIn, email } = props
-    const navigate = useNavigate();
-    
-    const onButtonClick = () => {
-        // You'll update this function later
-    }
+function Home() {
+  const keycloak = useContext(KeycloakContext);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-    return <div className="mainContainer">
-        <div className={"titleContainer"}>
-            <div>Welcome!</div>
-        </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (keycloak) {
+          console.log('User Authenticated: ' + keycloak.authenticated);
+          try {
+            const refreshed = await keycloak.updateToken(5);
+            console.log(refreshed ? 'Token was refreshed' : 'Token is still valid');
+            console.log('Token: ' + keycloak.token);
+          } catch (error) {
+              console.error('Failed to refresh the token:', error);
+          }
+          const response = await axios.get('http://localhost:8080/api/runs/test', {
+            headers: {
+              Authorization: `Bearer ${keycloak.token}`,
+            },
+          });
+          setData(response.data);
+        }
+      } catch (error) {
+        setError(error.message || 'An error occurred while fetching data.');
+      }
+    };
+
+    fetchData();
+  }, [keycloak]);
+
+  return (
+    <div>
+      {/* Display fetched data or error */}
+      {data ? (
         <div>
-            This is the home page.
+          <h2>Data from Backend:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
-        <div className={"buttonContainer"}>
-            <input
-                className={"inputButton"}
-                type="button"
-                onClick={onButtonClick}
-                value={loggedIn ? "Log out" : "Log in"} />
-            {(loggedIn ? <div>
-                Your email address is {email}
-            </div> : <div/>)}
-        </div>
-
-
+      ) : (
+        <p>Error: {error}</p>
+      )}
     </div>
+  );
 }
 
-export default Home
+export default Home;
